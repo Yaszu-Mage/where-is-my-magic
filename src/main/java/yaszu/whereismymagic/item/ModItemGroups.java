32b50b459006e903594f9df797acae9831f3293a
Yaszu-Mage <sunguildstudios@gmail.com>
2024-09-16 20:54:55 -0700

    you can now eat bleach

---
+++ src/main/java/yaszu/whereismymagic/item/ModItemGroups.java
@@ -0,0 +1,27 @@
+package yaszu.whereismymagic.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import org.slf4j.LoggerFactory;
+import yaszu.whereismymagic.WhereIsMyMagic;
+
+public class ModItemGroups {
+
+    public static final ItemGroup MAGIC_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(WhereIsMyMagic.MOD_ID, "magic"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.magic"))
+                    .icon(() -> new ItemStack(ModItems.CRACK)).entries((displayContext, entries) ->  {
+
+                    }).build());
+
+
+    public static void registerItemGroups() {
+        WhereIsMyMagic.LOGGER.info("Reigstering Item Groups for " + WhereIsMyMagic.MOD_ID);
+    }
+
+}
--- src/main/java/yaszu/whereismymagic/item/ModItems.java
+++ src/main/java/yaszu/whereismymagic/item/ModItems.java
@@ -3,6 +3,7 @@ package yaszu.whereismymagic.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.FoodComponents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -12,16 +13,20 @@ import yaszu.whereismymagic.WhereIsMyMagic;
 
 public class ModItems {
 
-    public static final Item MANA_DUST = registerItem("mana_dust", new Item(new FabricItemSettings()));
-    public static final Item CRACK = registerItem("crack", new Item(new FabricItemSettings()));
+    public static final Item MANA_DUST = registerItem("mana_dust", new Item(new FabricItemSettings().food((FoodComponents.SUSPICIOUS_STEW))));
+    public static final Item CRACK = registerItem("crack", new Item(new FabricItemSettings().food((FoodComponents.SUSPICIOUS_STEW))));
     public static final Item SPONGE_BONG = registerItem("sponge_bong", new Item(new FabricItemSettings()));
-    public static final Item BLEACH = registerItem("bleach", new Item(new FabricItemSettings()));
+    public static final Item BLEACH = registerItem("bleach", new Item(new FabricItemSettings().food((FoodComponents.POISONOUS_POTATO))));
+    public static final Item RAW_CRACK = registerItem("raw_crack", new Item(new FabricItemSettings()));
+
     private static void addItemstoIngredientTabItemGroup(FabricItemGroupEntries entries){
         entries.add(MANA_DUST);
         entries.add(CRACK);
         entries.add(SPONGE_BONG);
         entries.add(BLEACH);
+        entries.add(RAW_CRACK);
     }
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(WhereIsMyMagic.MOD_ID, name), item);
